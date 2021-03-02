02b49d05efc890175e61ea014314c99c1061a5c9
chronicallyunfunny <48951297+chronicallyunfunny@users.noreply.github.com>
2021-03-02 22:12:51 +0800

    Update NodeSniper.java
    Ternary cause cool

--- src/main/java/me/chronicallyunfunny/NodeSniper.java
+++ src/main/java/me/chronicallyunfunny/NodeSniper.java
@@ -11,10 +11,7 @@ public class NodeSniper {
     public static void main(String[] args) {
         try {
             Sniper sniper;
-            if (isMicrosoftAccount())
-                sniper = new MSASniper();
-            else
-                sniper = new MojangSniper();
+            sniper = (isMicrosoftAccount()) ? new MSASniper() : new MojangSniper();
             sniper.printSplashScreen();
             System.out.println("Initialising...");
             System.out.println();
