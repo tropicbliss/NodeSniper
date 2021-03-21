62d4d0035b3ad3a0d65dc7103feea0def9e35a31
chronicallyunfunny <test@example.com>
2021-03-21 21:57:25 +0800

    Hi

--- src/main/java/me/chronicallyunfunny/GCSniper.java
+++ src/main/java/me/chronicallyunfunny/GCSniper.java
@@ -63,7 +63,7 @@ public class GCSniper implements Sniper {
         System.out.println("Please make sure that your snipe will not last more than a day or the snipe will fail.");
         System.out.print(
                 "Sign in with your Microsoft account and copy the ID from the \"access_token\" field right here: ");
-        authToken = scanner.nextLine().replaceAll("\\s+", "").replaceAll("[\"]", "");
+        authToken = scanner.nextLine().strip().replaceAll("[\"]", "");
     }
 
     @Override
@@ -79,7 +79,7 @@ public class GCSniper implements Sniper {
         input = scanner.nextLine();
         if (input.isEmpty())
             return false;
-        giftCode = input.replaceAll("\\s+", "");
+        giftCode = input.strip();
         return true;
     }
 
@@ -120,7 +120,7 @@ public class GCSniper implements Sniper {
     @Override
     public void getUsernameChoice() {
         System.out.print("What name will you like to snipe: ");
-        snipedUsername = scanner.nextLine().replaceAll("\\s+", "");
+        snipedUsername = scanner.nextLine().strip();
         if ((snipedUsername.length() < 3) || (snipedUsername.length() > 16)
                 || (!(snipedUsername.matches("[A-Za-z0-9_]+"))))
             throw new GeneralSniperException("[GetUsernameChoice] You entered an invalid username.");
@@ -133,13 +133,13 @@ public class GCSniper implements Sniper {
         var yaml = new Yaml();
         Map<String, Object> accountData = yaml.load(actual);
         spread = (int) accountData.get("spread");
-        skinVariant = ((String) accountData.get("skinModel")).toLowerCase().replaceAll("\\s+", "");
+        skinVariant = ((String) accountData.get("skinModel")).toLowerCase().strip();
         isAutoOffset = (boolean) accountData.get("autoOffset");
         isChangeSkin = (boolean) accountData.get("changeSkin");
         if (isChangeSkin)
             if (!((skinVariant.equals("slim")) || (skinVariant.equals("classic"))))
                 throw new GeneralSniperException("[ConfigParser] Invalid skin type.");
-        skinPath = ((String) accountData.get("skinFileName")).replaceAll("\\s+", "");
+        skinPath = ((String) accountData.get("skinFileName")).strip();
         if (!(isAutoOffset)) {
             offset = (int) accountData.get("offset");
             System.out.println("Offset is set to " + offset + " ms.");
