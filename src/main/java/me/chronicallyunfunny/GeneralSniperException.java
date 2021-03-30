eae61be6756a5b4cd5ee557b8bc94211845b99d7
Eugene Toh <eugenetoh54@gmail.com>
2021-03-30 17:53:10 +0800

    e

--- src/main/java/me/chronicallyunfunny/GeneralSniperException.java
+++ src/main/java/me/chronicallyunfunny/GeneralSniperException.java
@@ -1,11 +1,6 @@
 package me.chronicallyunfunny;
 
 public class GeneralSniperException extends RuntimeException {
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
-
     public GeneralSniperException(String message) {
         super(message);
     }
